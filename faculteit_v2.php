1e9bd6ecb905f26749ff74aca593c5fc7ca0b7f3
NielzosFilms <56300732+NielzosFilms@users.noreply.github.com>
2020-02-03 10:21:55 +0100

    Add files via upload

---
+++ faculteit_v2.php
@@ -0,0 +1,13 @@
+<?php
+echo("Van welk getal wil je de faculteit weten?".PHP_EOL);
+$num = readline();
+$out = 1;
+$i = 1;
+while($i < $num){
+    $out += $out * $i;
+    $i++;
+}
+
+echo($out);
+
+?>
